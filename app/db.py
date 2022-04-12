4ad93b2a2baae1309b315606c8d319d5f6713bb3
kevinari26 <kevin_arisaputra@yahoo.com>
2022-04-12 17:48:10 +0700

    fungsi register dan add utang

--- app/db.py
+++ app/db.py
@@ -27,43 +27,167 @@ def db_drop_and_create_all():
     db.create_all()
 
 
-class DaftarUtang(db.Model):
-    __tablename__ = 'daftar_utang'
-    id = db.Column(db.Integer, primary_key = True)
-    u_id = db.Column(db.String(50))
-    debtor = db.Column(db.String(100))
-    price = db.Column(db.Float)
+class DaftarUser(db.Model): # tabel daftar user yang sudah register ke bot
+    __tablename__ = 'daftar_user'
+    id_user = db.Column(db.Integer, primary_key = True) # nomor baris sekaligus nomor id user
+    id_line = db.Column(db.String(50)) # id user
+    username = db.Column(db.String(20)) # username
+    
+    def __init__(self, id_line, username): # constructor
+        self.id_line = id_line
+        self.username = username
 
-    def __init__(self, u_id, debtor, price):
-        self.u_id = u_id
-        self.debtor = debtor
-        self.price = price
-
-    def insert(self):
+    def insert(self): # insert ke database
         db.session.add(self)
         db.session.commit()
 
-    def delete(self):
+    def delete(self): # delete dari database
         db.session.delete(self)
         db.session.commit()
 
-    def update(self):
+    def update(self): # update isi database
         db.session.commit()
 
-    def __str__(self):
+    @classmethod
+    def searchUser(cls, stringToSearch):
+        return cls.query.filter( # search id_line atau username sesuai stringToSearch
+            (cls.id_line == stringToSearch) |
+            (cls.username == stringToSearch)
+        ).all()
+    
+    def __str__(self): # print database
         d = {}
         for column in self.__table__.columns:
             d[column.name] = str(getattr(self, column.name))
         return str(d)
 
 
-def addUtang(user_id, msg):
-    datas = msg.split(" ")
-    debtor = datas[1]
-    price = float(datas[2])
-    
-    DaftarUtang(user_id, debtor, price).insert()
+class DaftarUtang(db.Model): # tabel daftar utang
+    __tablename__ = 'daftar_utang'
+    nomor = db.Column(db.Integer, primary_key = True) # nomor baris
+    id_lender = db.Column(db.Integer) # id pemberi utang
+    id_debtor = db.Column(db.Integer) # id peminjam
+    komen = db.Column(db.String(30)) # username
+    price = db.Column(db.Float) # besar utang
+    lunas = db.Column(db.Boolean) 
 
+    def __init__(self, id_lender, id_debtor, komen, price, lunas): # constructor
+        self.id_lender = id_lender
+        self.id_debtor = id_debtor
+        self.komen = komen
+        self.price = price
+        self.lunas = lunas
+
+    def insert(self): # insert ke database
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self): # delete dari database
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self): # update isi database
+        db.session.commit()
+
+    @classmethod
+    def total(cls, stringToSearch):
+        return cls.query.filter( # search id_line atau username sesuai stringToSearch
+            (cls.id_line == stringToSearch) |
+            (cls.username == stringToSearch)
+        ).all()
+    
+    '''
+    @classmethod
+    def get_all(cls, group_id):
+        return cls.query.with_entities(
+            cls.item_name,
+            cls.owner,
+            cls.price,
+            cls.created_at,
+        ).filter(
+            (cls.group_id == group_id) &
+            (cls.is_calced.is_(False))
+        ).order_by(
+            cls.created_at,
+        ).all()
+
+    @classmethod
+    def get_all_aggr(cls, group_id, item_name):
+        # select taker, sum(amount) where item_name and group_id group by taker
+        return cls.query.with_entities(
+            cls.taker,
+            db.func.sum(cls.amount).label("amount"),
+        ).filter(
+            (cls.group_id == group_id) &
+            (cls.item_name == item_name)
+        ).group_by(
+            cls.taker,
+        ).all()
+
+    @classmethod
+    def get_all(cls, group_id, item_name):
+        # select taker, sum(amount) where item_name and group_id group by taker
+        return cls.query.with_entities(
+            cls.taker,
+            cls.amount,
+            cls.created_at,
+        ).filter(
+            (cls.group_id == group_id) &
+            (cls.item_name == item_name)
+        ).all()
+    '''
+
+    def __str__(self): # print database
+        d = {}
+        for column in self.__table__.columns:
+            d[column.name] = str(getattr(self, column.name))
+        return str(d)
+
+
+
+# fungsi untuk akses database
+def register(id_line, in_string):
+    listUser = DaftarUser.searchUser (id_line) # cek apakah user sudah register
+    if (len(listUser)): # jika sudah register
+        return "sudah register"
+    else: # jika belum register, simpan data registrasi
+        username = in_string.split(" ")[1]
+        # insert ke database
+        DaftarUser(id_line, username).insert()
+        return "register berhasil"
+    # return (listUser[0])
+
+def addUtang(id_line, msg):
+    # nomor = Integer
+    # id_lender = Integer
+    # id_debtor = Integer
+    # komen = String(30)
+    # price = Float
+    # lunas = Boolean 
+    # add <username target> <komentar> <harga>
+
+    datas = msg.split(" ")
+    id_lender = 0
+    id_debtor = 0
+    komen = datas[2]
+    price = float(datas[3])
+
+    cekLender = DaftarUser.searchUser (id_line) # cek apakah lender sudah register
+    cekDebtor = DaftarUser.searchUser (datas[1]) # cek apakah debtor sudah register
+    if (len(cekLender) & len(cekDebtor)):
+        id_lender = cekLender[0].id_user
+        id_debtor = cekDebtor[0].id_user
+        # insert utang ke database
+        DaftarUtang(id_lender, id_debtor, komen, price, False).insert()
+
+def total(id_line):
+    cekUser = DaftarUser.searchUser (id_line) # cek apakah user sudah register
+    if (len(cekUser)): # jika user sudah register
+
+
+
+def getUser():
+    return DaftarUser.query.all()
 
 def getUtang():
     return DaftarUtang.query.all()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+'''
+akses db: http://127.0.0.1:5000/tes/getall/
+'''
+
+
 import os
-from app.db import addUtang, db_drop_and_create_all, getUtang, setup_db
+from tkinter import Y
+from app.db import register, addUtang, getUser, getUtang, setup_db, db_drop_and_create_all
 from flask import Flask, request, abort
 from flask_cors import CORS
 from linebot import LineBotApi, WebhookHandler
@@ -12,16 +18,19 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    # reset db
     @app.route("/tes/reset_db/")
     def reset_db():
         db_drop_and_create_all()
         return 'OK'
 
 
+
+    # handler untuk line
     line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
     handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
-
+    
     @app.route("/callback", methods=['POST'])
     def callback():
         signature = request.headers['X-Line-Signature']
@@ -62,8 +71,27 @@ def create_app(test_config=None):
                 TextSendMessage(text=msg)
             )
 
-    @app.route("/tes/getall/")
-    def getall():
+
+
+
+    # handler manual
+    @app.route("/tes/reg/")
+    def reg():
+        x = register("12354", "register ari")
+        x = register("12345", "register andy")
+        x = register("123456", "register sebas")
+
+        return "OK %s" % (x)
+
+    @app.route("/tes/tab1/") # print tabel user
+    def getall1():
+        x = getUser()
+        for i in x:
+            print(i)
+        return 'OK' + ' ' + "".join(map(str, x))
+    
+    @app.route("/tes/tab2/") # print tabel utang
+    def getall2():
         x = getUtang()
         for i in x:
             print(i)
@@ -71,7 +99,9 @@ def create_app(test_config=None):
     
     @app.route("/tes/add/")
     def addutang():
-        x = addUtang(1, "add andy 100")
+        x = addUtang("12354", "add andy nasi 100")
         return 'OK'
+    
+
 
     return app
