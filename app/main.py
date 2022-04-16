3e95375dbf7f0f379f9922a9bd19ab75aba95e66
kevinari26 <kevin_arisaputra@yahoo.com>
2022-04-16 12:14:13 +0700

    tes4

--- app/main.py
+++ app/main.py
@@ -102,24 +102,25 @@ def create_app(test_config=None):
         elif (msg == "help"):
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage (text = "halo"),
+                # TextSendMessage (text = "halo"),
+                messages=[
                 TemplateSendMessage(
-                        alt_text='Confirm template',
-                        template=ConfirmTemplate(
-                            text='Are you sure?',
-                            actions=[
-                                PostbackAction(
-                                    label='postback',
-                                    display_text='postback text',
-                                    data='action=buy&itemid=1'
-                                ),
-                                MessageAction(
-                                    label='message',
-                                    text='message text'
-                                )
-                            ]
-                        )
+                    alt_text='Confirm template',
+                    template=ConfirmTemplate(
+                        text='Are you sure?',
+                        actions=[
+                            PostbackAction(
+                                label='postback',
+                                display_text='postback text',
+                                data='action=buy&itemid=1'
+                            ),
+                            MessageAction(
+                                label='message',
+                                text='message text'
+                            )
+                        ]
                     )
+                )],
             )
         # def __handle_help(event, group_id, msg):
         # line_bot_api.reply_message(
